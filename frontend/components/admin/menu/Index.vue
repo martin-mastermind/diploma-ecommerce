4e6a47aaddbd96f9dfbe819e9b683c79909ac953
Beletskyi-Moawad Martin <m.beleckii@alef.im>
2023-05-06 19:11:19 +0300

    Добавлены роли админов

--- frontend/components/admin/menu/Index.vue
+++ frontend/components/admin/menu/Index.vue
@@ -1,18 +1,25 @@
-<script setup lang="ts" />
+<script setup lang="ts">
+import { useUser } from '~/store/admin/user'
+
+const { user } = useUser()
+
+const isContent = computed(() => user?.type === 'content' || user?.type === 'full')
+const isTech = computed(() => user?.type === 'tech' || user?.type === 'full')
+</script>
 
 <template>
   <nav class="border-r border-slate-300 flex flex-col">
     <section class="p-5 flex flex-col gap-5">
-      <AdminMenuLinkGroup title="Каталог">
+      <AdminMenuLinkGroup v-if="isContent" title="Каталог">
         <AdminMenuLink to="/admin/goods" icon="material-symbols:view-list-outline-rounded" title="Товары" />
         <AdminMenuLink to="/admin/categories" icon="material-symbols:category-outline-rounded" title="Категории" />
       </AdminMenuLinkGroup>
-      <AdminMenuLink class="text-xl" to="/admin/orders" icon="material-symbols:chrome-reader-mode-outline-rounded" title="Заказы" />
-      <AdminMenuLinkGroup title="Спец.предложения">
+      <AdminMenuLink v-if="isContent" class="text-xl" to="/admin/orders" icon="material-symbols:chrome-reader-mode-outline-rounded" title="Заказы" />
+      <AdminMenuLinkGroup v-if="isContent" title="Спец.предложения">
         <AdminMenuLink to="/admin/promotions" icon="material-symbols:info-outline-rounded" title="Акции" />
         <AdminMenuLink to="/admin/coupons" icon="mingcute:coupon-line" title="Купоны" />
       </AdminMenuLinkGroup>
-      <AdminMenuLink class="text-xl" to="/admin/appeals" icon="material-symbols:person-outline-rounded" title="Обращения" />
+      <AdminMenuLink v-if="isTech" class="text-xl" to="/admin/appeals" icon="material-symbols:person-outline-rounded" title="Обращения" />
     </section>
     <AdminMenuUser />
   </nav>
