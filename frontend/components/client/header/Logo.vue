09e31949f31cbf5039d07ed78f8bff332e093c09
Beletskyi-Moawad Martin <m.beleckii@alef.im>
2023-05-10 15:08:17 +0300

    [Client] Настроена шапка. Убраны комментарии

--- frontend/components/client/header/Logo.vue
+++ frontend/components/client/header/Logo.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts" />
 
 <template>
-  <div class="w-1/2 h-20 mx-auto">
+  <div class="w-1/2 h-16 mx-auto">
     <img class="w-full h-full object-contain" src="/img/logo.png" alt="">
   </div>
 </template>
