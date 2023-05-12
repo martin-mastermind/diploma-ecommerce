3be2da799a32bddde7c53f1f6fbe1bf5c3f8a10f
Beletskyi-Moawad Martin <m.beleckii@alef.im>
2023-05-12 15:20:20 +0300

    [Client] Убрана подсветка избранного по умолчанию

--- frontend/components/client/ui/CatalogItem.vue
+++ frontend/components/client/ui/CatalogItem.vue
@@ -34,7 +34,7 @@ async function openGood () {
         <Icon name="material-symbols:shopping-cart-outline-rounded" size="1.5rem" class="lg:w-7 lg:h-7" />
       </div>
       <div class="p-0.5 lg:p-1 rounded-full hover:bg-blue-100 active:bg-blue-200 transition-colors cursor-pointer">
-        <Icon name="material-symbols:favorite-outline-rounded" size="1.5rem" class="text-red-600 lg:w-7 lg:h-7" />
+        <Icon name="material-symbols:favorite-outline-rounded" size="1.5rem" class="lg:w-7 lg:h-7" />
       </div>
       <div class="p-0.5 lg:p-1 rounded-full hover:bg-blue-100 active:bg-blue-200 transition-colors cursor-pointer">
         <Icon name="material-symbols:candlestick-chart-outline-rounded" size="1.5rem" class="lg:w-7 lg:h-7" />
